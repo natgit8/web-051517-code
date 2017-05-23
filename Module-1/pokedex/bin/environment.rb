0274a3d74b1fec10e9279e84cdbe7f329148fa65
toneloke <david.guerrero87@live.com>
2017-05-23 10:29:57 -0400

    pokedex starter code

--- Module-1/pokedex/bin/environment.rb
+++ Module-1/pokedex/bin/environment.rb
@@ -1,11 +1,3 @@
 require 'bundler/setup'
 Bundler.require(:default, :development)
 require 'pry'
-
-require_relative "../lib/pokemon"
-require_relative "sql_runner"
-
-@db = SQLite3::Database.new('./db/pokemon.db')
-@db.execute("DROP TABLE IF EXISTS pokemon;")
-@sql_runner = SQLRunner.new(@db)
-@sql_runner.execute_schema_migration_sql
--- Module-1/pokedex/bin/run.rb
+++ Module-1/pokedex/bin/run.rb
@@ -1,3 +1,2 @@
-require_relative 'environment'
 
 binding.pry
